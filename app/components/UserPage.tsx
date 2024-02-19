dc7bb07f1cca886ad1bb5e54ec1a143bcf5758f8
Lukas Sandman <41347531+sandmanlukas@users.noreply.github.com>
2024-02-19 22:42:41 +0100

    Add beer dropdown.

--- app/components/UserPage.tsx
+++ app/components/UserPage.tsx
@@ -1,13 +1,15 @@
 "use client";
 
 import { getUser } from "@/app/lib/getUser";
-import { Spinner } from "@nextui-org/react";
+import { Button, Dropdown, DropdownItem, DropdownMenu, DropdownTrigger, DropdownSection, Spinner } from "@nextui-org/react";
 import { Item, Transaction, User } from "@prisma/client"
 import { useEffect, useState } from "react"
 import { handleScan } from "@/app/lib/utils";
 import { createTransaction } from "@/app/lib/createTransaction";
 import { toast } from "react-toastify";
 import Transactions from "./Transactions";
+import { getAllUsers } from "../lib/getAllUsers";
+import { set } from "zod";
 
 
 export interface TransactionWithItem extends Transaction {
@@ -16,6 +18,10 @@ export interface TransactionWithItem extends Transaction {
 
 export default function UserPage({ id }: { id: number }) {
     const [user, setUser] = useState<User | null>(null);
+    const [chosenUser, setChosenUser] = useState<User | null>(null);
+    const [currentUsers, setCurrentUsers] = useState<User[]>([]);
+    const [oldUsers, setOldUsers] = useState<User[]>([]);
+    const [otherUsers, setOtherUsers] = useState<User[]>([]);
     const [loading, setLoading] = useState(true);
     const [transactions, setTransactions] = useState<TransactionWithItem[]>([]);
     const [barcode, setBarcode] = useState<string | null>(null);
@@ -27,11 +33,18 @@ export default function UserPage({ id }: { id: number }) {
         const fetchUser = async () => {
             setLoading(true);
             const user = await getUser(id);
-            if (!user) {
+            const users = await getAllUsers();
+            if (!user || !users) {
                 setLoading(false);
                 return;
             }
             setUser(user);
+            const currentUsers = users.filter(filterUser => filterUser.role !== 'KADAVER' && filterUser.role !== 'OTHER' && filterUser.username !== user.username);
+            const oldUsers = users.filter(filterUser => filterUser.role === 'KADAVER' && filterUser.username !== user.username);
+            const otherUsers = users.filter(filterUser => filterUser.role !== 'OTHER' && filterUser.username !== user.username);
+            setCurrentUsers(currentUsers);
+            setOldUsers(oldUsers);
+            setOtherUsers(otherUsers);
             setTransactions(user.transactions);
             setDebt(user.debt);
             setLoading(false);
@@ -79,27 +92,65 @@ export default function UserPage({ id }: { id: number }) {
         performTransaction();
     }, [scanCount, user])
 
-
-
     return (
         loading ?
             <div className='mx-auto mt-2'>
                 <Spinner />
             </div>
             : (
-                <div className='mx-auto p-3'>
+                <div className="mx-auto p-3">
                     {user &&
                         <>
                             <div>
                                 <div className="flex justify-between items-baseline">
-                                    <h1 className="text-2xl font-bold mb-1">{user.username}</h1>
-                                    <p className="font-bold">{debt} kr</p>
+                                    <div>
+                                        <h1 className="text-2xl font-bold mb-1">{user.username}</h1>
+                                        <p className="text-sm">{user.firstName} {user.lastName}</p>
+                                    </div>
+
+                                    {(currentUsers || oldUsers || otherUsers) && (
+
+                                        <Dropdown>
+                                            <DropdownTrigger>
+                                                <Button
+                                                    size="lg"
+                                                    variant="bordered"
+                                                >
+                                                    Bärsa!
+                                                </Button>
+                                            </DropdownTrigger>
+                                            <DropdownMenu variant="faded" aria-label="Bärsa!">
+                                                <DropdownSection title="Sittande" showDivider>
+                                                    {currentUsers.map((filteredUser) => (
+                                                        <DropdownItem key={filteredUser.id} onClick={() => setChosenUser(filteredUser)}>
+                                                            {filteredUser.username}
+                                                        </DropdownItem>
+                                                    ))}
+                                                </DropdownSection>
+                                                <DropdownSection title="Kadaver" showDivider>
+                                                    {oldUsers.map((filteredUser) => (
+                                                        <DropdownItem key={filteredUser.id} onClick={() => setChosenUser(filteredUser)}>
+                                                            {filteredUser.username}
+                                                        </DropdownItem>
+                                                    ))}
+                                                </DropdownSection>
+                                                <DropdownSection title="Andra" >
+                                                    {otherUsers.map((filteredUser) => (
+                                                        <DropdownItem key={filteredUser.id} onClick={() => setChosenUser(filteredUser)}>
+                                                            {filteredUser.username}
+                                                        </DropdownItem>
+                                                    ))}
+                                                </DropdownSection>
+                                            </DropdownMenu>
+                                        </Dropdown>
+
+                                    )}
                                 </div>
-                                <p className="text-md">{user.firstName} {user.lastName}</p>
+                            </div>
+                            <div className="mt-3">
+                                <p><span className="font-bold">Skuld:</span> {debt} kr</p>
                             </div>
 
-                            <div>
-                            </div>
                             <Transactions transactions={transactions} />
                         </>
                     }
