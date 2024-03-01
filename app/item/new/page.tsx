295e25f4a81b5f0d8295447d72655bbea4d8cf79
Lukas Sandman <41347531+sandmanlukas@users.noreply.github.com>
2024-03-01 18:32:22 +0100

    Allow regular users to visit /item/new

--- app/item/new/page.tsx
+++ app/item/new/page.tsx
@@ -1,14 +1,7 @@
 import React from 'react';
-import { redirect } from 'next/navigation';
-
-import { auth } from '@/auth';
 import AddItemForm from '../../components/AddItemForm';
 
 async function AddItemPage({ children, }: { children: React.ReactNode }) {
-
-    const session = await auth();
-
-    if (!session || session.user.role != 'ADMIN') return redirect('/');
 
     return (
         <AddItemForm />
