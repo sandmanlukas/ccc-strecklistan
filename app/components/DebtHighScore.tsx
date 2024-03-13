257ba4cb558f9698f6546274431a9ffa41c4fdc9
Lukas Sandman <41347531+sandmanlukas@users.noreply.github.com>
2024-03-13 21:43:43 +0100

    Show debt highscore only if debt is greater than 0

--- app/components/DebtHighScore.tsx
+++ app/components/DebtHighScore.tsx
@@ -6,12 +6,13 @@ import React from 'react';
 
 export default function DebtHighScore({ users }: { users: User[] }) {
     const sortedUsers = users.sort((a, b) => b.debt - a.debt).slice(0, 5);
+    const highestDebt = sortedUsers[0]?.debt;
 
     return (
         <div className='ml-4'>
             <h3 className='text-2xl ml-2 mt-2 font-bold'>Skuldtopplista</h3>
             <div className='flex flex-col items-start mt-2'>
-                {sortedUsers.length > 0 ? (
+                {sortedUsers.length > 0 && highestDebt > 0 ? (
                     sortedUsers.map((user, index) => (
                         <div key={user.id} className='flex items-center justify-between w-96 bg-white shadow-md rounded-lg p-2 mb-2 border border-grey'>
                             <Link href={`user/${user.id}`} ><h3 className='text-lg font-medium'>{index + 1}. {user.username}</h3></Link>
@@ -20,7 +21,7 @@ export default function DebtHighScore({ users }: { users: User[] }) {
                     ))) :
                     (
                         <div className='flex items-center justify-between w-96 bg-white shadow-md rounded-lg p-2 mb-2 border border-grey'>
-                            <h3 className='text-lg font-medium'>Ingen data än. Strecka något.</h3>
+                            <h3 className='text-lg font-medium'>Ingen data än eller så har ingen en skuld. Strecka något.</h3>
                         </div>
                     )}
             </div>
