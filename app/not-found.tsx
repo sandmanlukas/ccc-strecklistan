e8ee7fc33809e77f70e285cd3b70d0aca932831a
Lukas Sandman <41347531+sandmanlukas@users.noreply.github.com>
2024-03-16 16:44:02 +0100

    Update not found page.

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,11 +1,13 @@
 import Link from 'next/link'
- 
+
 export default function NotFound() {
   return (
-    <div>
-      <h2>Not Found</h2>
-      <p>Could not find requested resource</p>
-      <Link href="/">Return Home</Link>
+    <div className="mt-10 w-full flex flex-col items-center justify-center">
+      <h2 className="text-4xl font-bold text-red-500 mb-4">Sidan finns ej!</h2>
+      <p className="text-xl text-gray-700 mb-8">Den här sidan finns ej, ska du inte strecka en öl istälet?</p>
+      <Link href="/" className='text-blue-500 hover:underline'>
+        Hem igen
+      </Link>
     </div>
   )
 }
