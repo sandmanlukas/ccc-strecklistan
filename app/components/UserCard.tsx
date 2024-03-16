2587df45483a236deac95b41e10a5bb8c66870f5
Lukas Sandman <41347531+sandmanlukas@users.noreply.github.com>
2024-03-16 16:40:30 +0100

    Add fxed size to user card.

--- app/components/UserCard.tsx
+++ app/components/UserCard.tsx
@@ -13,14 +13,16 @@ function UserCard({ user }: { user: User }) {
     const isUserActive = !user.role.includes('KADAVER') && !user.role.includes('OTHER');
     return (
         <Link href={`/user/${user.id}`} as={`/user/${user.id}`}>
-            <div className="shadow border rounded  bg-white min-h-[100px] justify-between p-4 overflow-clip cursor-pointer transition-all ease-in hover:shadow ">
+            <div className="shadow border rounded  bg-white min-h-[100px] flex flex-col justify-between p-4 overflow-clip cursor-pointer transition-all ease-in hover:shadow w-[250px] h-[300px] ">
                 {
                     <Image alt="Användarens avatar" src={user.avatar ? user.avatar : DEFAULT_AVATAR_URL} width={250} height={250} className='rounded' />
                 }
-                <p className='text-xl font-bold mb-2'>{user.username}</p>
-                <p className='text-sm text-left mb-0'>{user.firstName} {user.lastName} {isUserActive && (<span>
-                    - {positionLabels[user.role]}
-                </span>)}</p>
+                <div>
+                    <p className='text-xl font-bold mb-2'>{user.username}</p>
+                    <p className='text-sm text-left mb-0'>{user.firstName} {user.lastName} {isUserActive && (<span>
+                        - {positionLabels[user.role]}
+                    </span>)}</p>
+                </div>
             </div>
         </Link>
     );
