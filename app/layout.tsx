c62df29c0e5d84e2e45fd3944fe23c53103fd8de
Lukas Sandman <41347531+sandmanlukas@users.noreply.github.com>
2024-03-17 19:47:15 +0100

    Add vercel speed insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import 'react-toastify/dist/ReactToastify.css';
 import type { Metadata } from "next";
 import { SessionProvider } from "next-auth/react";
 import { ToastContainer } from "react-toastify";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 import Navbar from "@/app/components/Navbar";
 import { auth } from "@/auth";
@@ -43,6 +44,7 @@ export default async function RootLayout({
             <main className="mx-auto max-w-5xl text-xl flex">
               {children}
             </main>
+            <SpeedInsights />
           </SessionProvider>
         </Providers>
       </body>
