4cc4167fed11b699a08a2457954fa6b98d9c9a8e
Lukas Sandman <41347531+sandmanlukas@users.noreply.github.com>
2024-03-17 18:12:51 +0100

    Add default avatar to public folder instead of blob storage

--- app/constants.ts
+++ app/constants.ts
@@ -1,2 +1,2 @@
-export const DEFAULT_AVATAR_URL = "https://m1jd2y7yyifuzhde.public.blob.vercel-storage.com/default-avatar"
+export const DEFAULT_AVATAR_URL = "/default_avatar.png";
 export const BEERED_BARCODE = "0000000000000";
