e921a73da7558df297e8718cca3212193796038b
Lukas Sandman <41347531+sandmanlukas@users.noreply.github.com>
2024-03-21 20:27:19 +0100

    Don't show beered item in item list

--- app/lib/getAllItems.ts
+++ app/lib/getAllItems.ts
@@ -1,11 +1,16 @@
 "use server";
 
+import { BEERED_BARCODE } from "../constants";
 import { prisma } from "./db";
 
 async function getAllItems() {
-    const items = await prisma.item.findMany(
-        {
-            orderBy: {
+    const items = await prisma.item.findMany({
+        where: {
+            barcode: {
+                not: BEERED_BARCODE
+            }
+        },
+        orderBy: {
                 name: 'asc'
             }
         }
