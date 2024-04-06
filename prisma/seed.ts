b8ce1cca9c1d7412cf746f97f39476cfe57692d7
Lukas Sandman <lukas.sandman@burtcorp.com>
2024-04-06 18:34:29 +0200

    Update seed to create items and transactions.

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,4 +1,4 @@
-import { AccountRole, UserRole } from "@prisma/client";
+import { AccountRole, Item, User, UserRole } from "@prisma/client";
 import { prisma } from "../app/lib/db";
 import { faker } from "@faker-js/faker";
 const bcrypt = require("bcryptjs");
@@ -27,17 +27,59 @@ export function createRandomUser(role: UserRole): FakeUser {
 }
 
 async function createAndAddUsersToDatabase() {
+  let users = [];
   const roles = Object.values(UserRole);
   for (let i = 0; i < 10; i++) {
     const role = roles[Math.floor(Math.random() * roles.length)];
     const randomUser = createRandomUser(role); // You can specify the role here
     // Add the user to the database using your database logic
     // Example: await prisma.user.create({ data: randomUser });
-    await prisma.user.create({ data: randomUser });
+    const user = await prisma.user.create({ data: randomUser });
+    users.push(user);
+  }
+  return users;
+}
+
+async function createAndAddItemsToDatabase() {
+  let items = [];
+  for (let i = 0; i < 10; i++) {
+    const randomItem = {
+      name: faker.commerce.productName(),
+      price: Number(faker.commerce.price()), // Convert price to number
+      volume: faker.number.int({min: 1, max: 100}),
+      barcode: faker.string.uuid(),
+    };
+    // Add the item to the database using your database logic
+    // Example: await prisma.item.create({ data: randomItem });
+    const item =  await prisma.item.create({ data: randomItem });
+    items.push(item);
+  }
+  return items;
+}
+
+async function createFakeTransactions(items: Item[], users: User[]) {
+  for (let i = 0; i < 30; i++) {
+    const barcode = items[Math.floor(Math.random() * items.length)].barcode;
+    const user = users[Math.floor(Math.random() * users.length)].id;
+    const randomTransaction = {
+      userId: user,
+      barcode: barcode,
+      beeredTransaction: false,
+      price: faker.number.int({min:15, max: 100}),
+      beeredUser: null,
+      beeredBy: null,
+    };
+
+    await prisma.transaction.create({ data: randomTransaction });
   }
 }
 
 async function main() {
+  await prisma.account.deleteMany({});
+  await prisma.user.deleteMany({});
+  await prisma.item.deleteMany({});
+  await prisma.transaction.deleteMany({});
+
   const hashedPassword = await bcrypt.hash(
     process.env.ADMIN_PASSWORD || "admin",
     10
@@ -103,7 +145,9 @@ async function main() {
     },
   });
 
-  await createAndAddUsersToDatabase();
+  const users = await createAndAddUsersToDatabase();
+  const items = await createAndAddItemsToDatabase();
+  await createFakeTransactions(items, users);
 }
 
 main()
