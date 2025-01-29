8ed1771757929988ed4332a70c9f07ea8a93e91d
Lukas Sandman <41347531+sandmanlukas@users.noreply.github.com>
2025-01-29 21:02:02 +0100

    Send emails in the background
    Now the debt emails will be sent in the background instead of us having
    to wait for the route to finish. This will hopefully prevent it from
    crashing.

--- app/api/send/route.ts
+++ app/api/send/route.ts
@@ -18,35 +18,35 @@ interface RequestData {
 export async function POST(req: Request) {
     try {
         const requestData: RequestData = await req.json();
-        const body = requestData.body;
-        const subject = requestData.subject;
-        const lastEmailSent = requestData.lastEmailSent;
-        const swish = requestData.swish;
-        const from = process.env.EMAIL_ADDRESS as string;
 
-        const emails = requestData.users.map((user: UserWithItemsAndTransactions) => {
-            return {
-                from: from,
-                to: [user.email],
-                subject: subject,
-                react: Email({ body: body, debt: user.debt, transactions: user.transactions, lastEmailSent, swish}) as React.ReactElement,
-            }
-        });
+        sendEmailsInBackground(requestData);
 
-        const emailBatches = [];
-        let response = null;
-        for (let i = 0; i < emails.length; i += 5) {
-            emailBatches.push(emails.slice(i, i + 5));
-        }
-
-        for (const batch of emailBatches) {
-            console.log('Sending batch of emails', batch);
-            response = await resend.batch.send(batch);
-        }
-
-        return Response.json(response);
+        return Response.json({ message: 'Email sending initiated' });
     } catch (error) {
         console.log('error', error);
         return Response.json({ error });
+    }
+}
+
+
+async function sendEmailsInBackground(data: RequestData) {
+    const { body, subject, lastEmailSent, swish, users } = data;
+    const from = process.env.EMAIL_ADDRESS as string;
+
+    const emails = users.map((user: UserWithItemsAndTransactions) => ({
+        from,
+        to: [user.email],
+        subject,
+        react: Email({ body, debt: user.debt, transactions: user.transactions, lastEmailSent, swish }) as React.ReactElement,
+    }));
+
+    const emailBatches = [];
+    for (let i = 0; i < emails.length; i += 5) {
+        emailBatches.push(emails.slice(i, i + 5));
+    }
+
+    for (const batch of emailBatches) {
+        console.log('Sending batch of emails', batch);
+        await resend.batch.send(batch);
     }
 }
